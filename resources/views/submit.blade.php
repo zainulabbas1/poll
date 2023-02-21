cb7ad419cdcb0b25659fa7492545cd456d48a751
zain abbas <zain.abbas@sixlogics.local>
2023-02-21 16:36:48 +0500

    update

--- resources/views/submit.blade.php
+++ resources/views/submit.blade.php
@@ -24,7 +24,8 @@ function myFunction() {
     copyText.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copyText.value);
     alert("Copied the text: " + copyText.value);
-    window.location.href = "{{ route('home') }}";}
+    // window.location.href = "{{ route('home') }}";
+}
 </script>
 
 
